9cf4081db401c8866c44a264ddd452dbbba13782
lmrae0624 <lmrae0624@gmail.com>
2022-01-16 21:24:46 +0900

    .

--- DFS와 BFS/감시 피하기2.py
+++ DFS와 BFS/감시 피하기2.py
@@ -9,16 +9,3 @@ for i in range(n):
     for j in range(n):
         if graph[i][j]=='T':
             teacher.append((i,j))
-
-def dfs(wall):
-    if wall==3:
-        for i in range(n):
-            for j in range(n):
-                tmp[i][j]=graph[i][j]
-        
-        for i in range(n):
-            for j in range(n):
-                if tmp[i][j]=='T':
-    
-                 
-
