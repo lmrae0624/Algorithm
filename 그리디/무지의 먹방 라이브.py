b74bca1ed75b92ced24188d742d7798136d3deae
lmrae0624 <lmrae0624@gmail.com>
2022-01-19 16:28:30 +0900

    .

--- 그리디/무지의 먹방 라이브.py
+++ 그리디/무지의 먹방 라이브.py
@@ -1,5 +1,5 @@
 import heapq
-from xxlimited import foo
+
 food_times=list(map(int,input().split()))
 k=int(input())
 
@@ -10,6 +10,29 @@ else:
     for i in range(len(food_times)):
         heapq.heappush(q,(food_times[i],i+1))
 
+    previous=0
+    sum_time=0
+    length=len(food_times)
 
+    while True:
+        min_value,index=heapq.heappop(q)
+        now=(min_value-previous)*length
+        if sum_time+now > k:
+            heapq.heappush(q,(min_value,index))
+            break
+        sum_time+=now
+        length-=1
+        previous=min_value
+
+    # while sum_value + ((q[0][0] - previous) * length) <= k:
+    #     now = heapq.heappop(q)[0]
+    #     sum_value += (now - previous) * length
+    #     length -= 1 # 다 먹은 음식 제외
+    #     previous = now # 이전 음식 시간 재설정
+    
+    result=sorted(q,key=lambda x:x[1])
+    print(q)
+    print(result[k-sum_time][1])   
+        
 # 3 1 2
 # 5
