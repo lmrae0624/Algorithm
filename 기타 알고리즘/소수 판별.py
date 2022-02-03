c9c7d493e84bc75042fcfbfa7e739fbcdf5dea7a
lmrae0624 <lmrae0624@gmail.com>
2022-02-03 14:04:53 +0900

    .

--- 기타 알고리즘/소수 판별.py
+++ 기타 알고리즘/소수 판별.py
@@ -2,7 +2,7 @@ from cmath import sqrt
 import math
 
 def is_prime_number(x):
-    for i in range(2,int(math.sqrt(x))+1):
+    for i in range(2,int(math.sqrt(x))+1): #int(x**0.5)+1
         if x%i==0:
             return False
     return True
