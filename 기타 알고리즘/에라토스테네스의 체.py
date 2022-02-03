149848585ff8eddd38851a324dacb85c45deb307
lmrae0624 <lmrae0624@gmail.com>
2022-02-03 14:31:28 +0900

    .

---
+++ 기타 알고리즘/에라토스테네스의 체.py
@@ -0,0 +1,15 @@
+import math
+
+n=1000
+array=[True]*(n+1)
+
+for i in range(2,int(math.sqrt(n))+1):
+    if array[i]:
+        j=2 #본인을 제외한 배수를 확인하기 위해서
+        while i*j<=n:
+            array[i*j]=False
+            j+=1
+
+for i in range(2,n+1):
+    if array[i]:
+        print(i,end=' ')
