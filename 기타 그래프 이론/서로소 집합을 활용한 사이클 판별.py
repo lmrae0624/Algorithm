ef462448db8ec6e970c771f5e4a61ac15a359c3c
lmrae0624 <lmrae0624@gmail.com>
2022-02-04 14:15:41 +0900

    .

---
+++ 기타 그래프 이론/서로소 집합을 활용한 사이클 판별.py
@@ -0,0 +1,38 @@
+def find_parent(parent,x): #루트 노드 찾기
+    if parent[x]!=x:
+        parent[x]=find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+v, e=map(int,input().split()) #노드, 간선(union 연산)
+parent=[0]*(v+1)
+
+for i in range(1,v+1):
+    parent[i]=i
+
+cycle=False
+for i in range(e):
+    a,b=map(int,input().split())
+    if find_parent(parent,a)==find_parent(parent,b):
+        cycle=True
+        break
+    else: 
+        union_parent(parent,a,b)
+    print(parent)
+print(parent)
+if cycle:
+    print('사이클 발생')
+else:
+    print('사이클 발생x')
+
+# 3 3 
+# 1 2 
+# 1 3
+# 2 3
