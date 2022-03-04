5d5b4b4662eadaa944b420f312ba96c6957eb841
lmrae0624 <lmrae0624@gmail.com>
2022-03-04 23:22:40 +0900

    .

--- 프로그래머스/약수의 개수와 덧셈.py
+++ 프로그래머스/약수의 개수와 덧셈.py
@@ -1,5 +1,11 @@
 def solution(left, right):
     answer = 0
+    for num in range(left,right+1):
+        if num%(num**0.5)==0:
+            answer-=num
+        else:
+            answer+=num
+
     return answer
 
-ㅔ갸ㅜㅅ
+print(solution(13,17))
--- 프로그래머스/카카오/문자열 압축.py
+++ 프로그래머스/카카오/문자열 압축.py
@@ -1,14 +1,34 @@
 def solution(s):
-    answer = 0
-    count=0
-    pre_s=s[0]
-    for i in range(len(s)//2+1):
-        for j in range(len(s)):
-            if pre_s==s[i]:
-                
-        s[i:]
+    answer = s
 
-    return answer
+    for i in range(1,len(s)//2+1): #자르는 개수 
+        index=0 #시작 인덱스 
+
+        pre_s=s[index:index+i] #이전값 저장
+        count=1
+
+        tmp=''
+        while True:
+            index+=i
+
+            if pre_s==s[index:index+i]:
+                count+=1
+            else:
+                if count>1:
+                    tmp+=str(count)+pre_s
+                else:
+                    tmp+=pre_s
+                count=1
+                pre_s=s[index:index+i]
+
+            if index+i>len(s):
+                tmp+=s[index:]
+                break
+
+        if len(answer)>len(tmp):
+            answer=tmp
+
+    return len(answer)
 
 
 print(solution("aabbaccc"))
