2d4388d945eedccc31dae88cfe5dd16294696313
lmrae0624 <lmrae0624@gmail.com>
2022-03-10 23:52:28 +0900

    .

--- 프로그래머스/카카오/__수식 최대화.py
+++ 프로그래머스/카카오/__수식 최대화.py
@@ -1,44 +1,58 @@
 def calculate(cal,a,b):
+    a=int(a)
+    b=int(b)
     if cal=='+':
-        return a+b
+        return str(a+b)
     elif cal=='-':
-        return a-b
+        return str(a-b)
     else:
-        return a*b
+        return str(a*b)
 
 def solution(expression):
     from itertools import permutations 
     answer = 0
     cal= ['+', '-', '*']
 
-    array=[]
+    num_array=[]
     tmp=''
-    for i in expression:
+    for i in expression: #num_array에 숫자랑 기호 끊어서 넣기
         if i.isdigit():
             tmp+=i
         else:
-            array.append(tmp)
-            array.append(i)
+            num_array.append(tmp)
+            num_array.append(i)
             tmp=''
-    array.append(tmp)
+    num_array.append(tmp)
 
+    for per in permutations(cal): 
+        array=num_array.copy() #list1=list2 하면 2의 값이 바뀔 때 1도 바뀌기 때문에 copy()
 
-    for per in permutations(cal):
-        new_array=array
-        for i in per:
-            for j in range(len(new_array)):
-                if new_array[j]==i:
-                    print(j,array[j])
-                    print(new_array)
-                    new_array[j-1]=calculate(new_array[j],int(new_array[j-1]),int(new_array[j+1]))
-                    new_array[j+1]=new_array[j-1]
+        for p in per:
+            stack=[]
+            while array:
+                tmp=array.pop(0)
+                if tmp==p:
+                    stack.append(calculate(p,stack.pop(),array.pop(0)))
+                else:
+                    stack.append(tmp)
+            array=stack
 
-        print(new_array)  
-        for a in new_array:
-            print(a)
-            if any(b.isdigit() for b in a):
-                answer=max(answer,abs(int(a)))  
-    print(array)
+        answer=max(answer,abs(int(array[0])))
+     
     return answer
 
 print(solution("100-200*300-500+20"))
+
+
+def solution(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
--- 프로그래머스/행렬 테두리 회전하기.py
+++ 프로그래머스/행렬 테두리 회전하기.py
@@ -1,11 +1,10 @@
 def solution(rows, columns, queries):
     array=[[i+rows*j for i in range(1,rows+1)] for j in range(columns)]
     answer = []
-
+ 
     for a,b,c,d in queries:
-        array[a-1][b-1]
+        print(a,b,c,d)
         
-
     return answer
 
 
