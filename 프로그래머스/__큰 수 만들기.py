2abc85597c02ad6a0c55d53937ae75678cf6197b
lmrae0624 <lmrae0624@gmail.com>
2022-03-10 17:26:09 +0900

    .

--- 프로그래머스/__큰 수 만들기.py
+++ 프로그래머스/__큰 수 만들기.py
@@ -1,8 +1,14 @@
 def solution(number, k):
-    answer = ''
-    return answer
+    answer = [number[0]]
 
+    for num in number[1:]:
+        while answer and k>0 and answer[-1]<num:
+            answer.pop()
+            k-=1
+        answer.append(num)
 
-print(solution("1924",2))
+    return "".join(answer[:len(answer)-k])
+
+print(solution("1943",2))
 print(solution("1231234",3))
 print(solution("4177252841",4))
