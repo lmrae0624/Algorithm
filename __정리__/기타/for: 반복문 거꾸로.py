8784ea9b849dec44d8f87cdf49e162c521e7f74e
lmrae0624 <lmrae0624@gmail.com>
2022-03-24 11:30:09 +0900

    .

---
+++ __정리__/기타/for: 반복문 거꾸로.py
@@ -0,0 +1,6 @@
+array=[1,2,3,4]
+for i in range(3,-1,-1):
+    print(array[i])
+
+for i in reversed(range(4)):
+    print(array[i])
--- 프로그래머스/***입국심사.py
+++ 프로그래머스/***입국심사.py
@@ -1,11 +1,38 @@
 def solution(n, times):
     answer = 0
+    start=1
+    end=max(times)*n #가장 오래걸리는 시간
 
-    while n>0:
-        n-=1
+    while start<=end:
+        mid=(start+end)//2 #이 시간동안 심사가 가능한지 판단하기
+        people=0
+
+        for t in times: 
+            people += mid // t # mid시간동안 심사 가능한 인원수 세기
         
-
+        if people >= n: #심사 가능
+            answer = mid
+            end = mid - 1
+        else: #심사 불가능
+            start = mid + 1
 
     return answer
 
 print(solution(6,[7,10]))
+
+def solution(n, times):
+    answer = 0
+    start, end, mid = 1, times[-1] * n, 0
+
+    while start < end:
+        mid = (start + end) // 2
+        total = 0
+        for time in times:
+            total += mid // time
+
+        if total >= n:
+            end = mid
+        else:
+            start = mid + 1
+    answer = start
+    return answer
