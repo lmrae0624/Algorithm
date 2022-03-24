9199ede4bcef25bba40a254d5f0adb8b74c0778e
lmrae0624 <lmrae0624@gmail.com>
2022-03-24 11:01:45 +0900

    .

--- 프로그래머스/***N으로 표현.py
+++ 프로그래머스/***N으로 표현.py
@@ -1,16 +1,29 @@
 def solution(N, number):
-    dp=[0]*(number+1)
+    answer = -1
+    dp=[]
 
-    for i in range(1,number+1):
-        dp[i]=dp[i-1]+2
-        
-        if i>=N:
-            if i%N==0:
-                dp[i]=min(dp[i],dp[i-N]+1)
-            else:
-                dp[i]=min(dp[i],dp[i//N*N]+2*(i%N))
-    print(dp)
-    return dp[number]
+    for i in range(1,9): #i는 N의 개수이고 최솟값이 8보다 크면 -1 리턴하기 때문에 1부터 9까지 반복
+
+        array=set()
+        array.add(int(str(N)*i))
+
+        for j in range(0,i-1): # i가 5일 경우 j는 마지막을 제외한 0,1,2,3까지 돌고
+            for x in dp[j]: #dp[0](N 1개로 만들 수 있는 숫자)을 또 반복       dp[0] d[1] dp[2] dp[3]
+                for y in dp[-j-1]: #dp[-1](N 4개로 만들 수 있는 숫자)을 반복  dp[-1] d[-2] dp[-3] dp[-4]
+                    array.add(x+y)
+                    array.add(x-y)
+                    array.add(x*y)
+
+                    if y!=0:
+                        array.add(x//y)
+
+        if number in array:
+            answer=i
+            break
+
+        dp.append(array)
+
+    return answer
 
 
 print(solution(5,12))
