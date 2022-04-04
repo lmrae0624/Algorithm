e2514618f0474abdf11df8ff2e9549ea6ae918af
lmrae0624 <lmrae0624@gmail.com>
2022-04-04 14:08:29 +0900

    .

--- 6. 다이나믹 프로그래밍/382) 편집 거리.py
+++ 6. 다이나믹 프로그래밍/382) 편집 거리.py
@@ -18,7 +18,7 @@ for i in range(1,n+1):
             d[i][j]=d[i-1][j-1]
         else:
             d[i][j]=min(d[i-1][j-1],d[i][j-1],d[i-1][j])+1
-#print(d)
+print(d)
 print(d[n][m])
 
 # saturday
