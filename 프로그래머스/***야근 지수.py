9409066b39cf50325d2356a63c113a7e91cf533a
lmrae0624 <lmrae0624@gmail.com>
2022-04-15 11:20:39 +0900

    .

---
+++ 프로그래머스/***야근 지수.py
@@ -0,0 +1,18 @@
+def solution(n, works):
+    import heapq
+    answer = 0
+
+    if sum(works)<=n:
+        return 0
+    
+    h=[]
+    for i in works:
+        heapq.heappush(h,-i)
+
+    for i in range(n):
+        heapq.heappush(h,heapq.heappop(h)+1)
+
+    return sum([i*i for i in h])
+
+print(solution(4,[4, 3, 3]))
+print(solution(1,[2, 1, 2]))
